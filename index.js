2a3d52c2321f3d82ca651c90baeb80d4c468b958
dev-kawser <kawserofficial.dev@gmail.com>
2024-08-15 10:53:21 +0600

    basic server created

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+    ]
+}));
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
